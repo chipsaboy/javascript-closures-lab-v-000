fab29dc901f2360c143142602b3d5d78000cd406
Nathan Campbell <nathancampbell@outlook.com>
2017-09-26 17:45:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  let counter = 0;
+
+  function addBump() {
+    counter ++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
